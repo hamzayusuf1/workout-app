e8970031a82b0745db0ab4233f18d8e3a79a5f7b
Hamza <hamzayusuf26@outlook.com>
2023-06-21 15:55:01 +0100

    added doc title and favicon

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,16 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Layout/Routes";
 import { AppContextProvider } from "./State/AppContext";
 import { Toaster } from "react-hot-toast";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="w-full mx-auto bg-workout-tertiary">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Workout App</title>
+        <link rel="canonical" href="https://exercisemadesimple.netlify.app/" />
+      </Helmet>
       <AppContextProvider>
         <RouterProvider router={router}></RouterProvider>
         <Toaster></Toaster>
